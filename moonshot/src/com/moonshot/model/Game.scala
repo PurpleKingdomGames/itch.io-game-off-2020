b4ec2e081fbf802b9748bf38561309536bf80d55
David North <hobnob@users.noreply.github.com>
2020-11-06 08:39:17 +0000

    Improves ship controls
    Movement is now based on frame tick rather than keypress, and uses acceleration to decide how fast the ship moves

--- moonshot/src/com/moonshot/model/Game.scala
+++ moonshot/src/com/moonshot/model/Game.scala
@@ -2,24 +2,45 @@ package com.moonshot.model
 
 import indigo._
 import indigo.shared.events.KeyboardEvent.KeyDown
+import indigo.shared.events.KeyboardEvent.KeyUp
 import indigo.shared.time.GameTime
 
 final case class Game(ship: Ship) {
 
   def update(gameTime: GameTime): GlobalEvent => Outcome[Game] = {
+    case FrameTick =>
+      Outcome(this.copy(ship = ship.update(gameTime)))
+
     case KeyDown(k) =>
       k match {
         case Key.RIGHT_ARROW =>
-          Outcome(this.copy(ship = ship.moveRight(gameTime)))
+          Outcome(this.copy(ship = ship.moveRight()))
 
         case Key.LEFT_ARROW =>
-          Outcome(this.copy(ship = ship.moveLeft(gameTime)))
+          Outcome(this.copy(ship = ship.moveLeft()))
 
         case Key.UP_ARROW =>
-          Outcome(this.copy(ship = ship.moveUp(gameTime)))
+          Outcome(this.copy(ship = ship.moveUp()))
 
         case Key.DOWN_ARROW =>
-          Outcome(this.copy(ship = ship.moveDown(gameTime)))
+          Outcome(this.copy(ship = ship.moveDown()))
+
+        case _ =>
+          Outcome(this)
+      }
+    case KeyUp(k) =>
+      k match {
+        case Key.RIGHT_ARROW =>
+          Outcome(this.copy(ship = ship.stopHorizontal()))
+
+        case Key.LEFT_ARROW =>
+          Outcome(this.copy(ship = ship.stopHorizontal()))
+
+        case Key.UP_ARROW =>
+          Outcome(this.copy(ship = ship.stopVertical()))
+
+        case Key.DOWN_ARROW =>
+          Outcome(this.copy(ship = ship.stopVertical()))
 
         case _ =>
           Outcome(this)
--- moonshot/src/com/moonshot/model/Ship.scala
+++ moonshot/src/com/moonshot/model/Ship.scala
@@ -2,20 +2,51 @@ package com.moonshot.model
 
 import indigo._
 
-final case class Ship(coords: Vector2) {
-  val speed = 600
+final case class Ship(coords: Vector2, currentSpeed:Vector2, targetSpeed: Vector2) {
+  val maxSpeed: Double = 150
+  val acceleration: Double = 25;
 
-  def moveRight(gameTime: GameTime) =
-    this.copy(coords = coords.withX((coords.x + (speed * gameTime.delta.value))))
+  def update(gameTime: GameTime) = {
+    val newSpeed = currentSpeed + ((targetSpeed - currentSpeed) * acceleration * gameTime.delta.value);
+    val clampedSpeed = new Vector2(
+      (
+        if (newSpeed.x > maxSpeed)
+          maxSpeed
+        else if (newSpeed.x < -maxSpeed)
+          -maxSpeed
+        else
+          newSpeed.x
+      ),
+      (
+        if (newSpeed.y > maxSpeed)
+          maxSpeed
+        else if (newSpeed.y < -maxSpeed)
+          -maxSpeed
+        else
+          newSpeed.y
+      ),
+    )
 
-  def moveLeft(gameTime: GameTime) =
-    this.copy(coords = coords.withX((coords.x + -(speed * gameTime.delta.value))))
+    this.copy(coords = coords + (clampedSpeed * gameTime.delta.value), currentSpeed = clampedSpeed)
+  }
 
-  def moveUp(gameTime: GameTime) =
-    this.copy(coords = coords.withY((coords.y + -(speed * gameTime.delta.value))))
+  def moveRight() =
+    this.copy(targetSpeed = targetSpeed.withX(maxSpeed))
 
-  def moveDown(gameTime: GameTime) =
-    this.copy(coords = coords.withY((coords.y + (speed * gameTime.delta.value))))
+  def moveLeft() =
+    this.copy(targetSpeed = targetSpeed.withX(-maxSpeed))
+
+  def moveUp() =
+    this.copy(targetSpeed = targetSpeed.withY(-maxSpeed))
+
+  def moveDown() =
+    this.copy(targetSpeed = targetSpeed.withY(maxSpeed))
+
+  def stopHorizontal() =
+    this.copy(targetSpeed = targetSpeed.withX(0))
+
+  def stopVertical() =
+    this.copy(targetSpeed = targetSpeed.withY(0))
 
   def toScreenSpace: Point =
     coords.toPoint
@@ -23,5 +54,5 @@ final case class Ship(coords: Vector2) {
 
 object Ship {
   val initial: Ship =
-    Ship(Vector2.zero)
+    Ship(Vector2.zero, Vector2.zero, Vector2.zero)
 }
