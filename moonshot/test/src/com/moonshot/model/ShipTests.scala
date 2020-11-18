b1ca5052665b02aac61d39568ffa72004ccb4c94
Dave Smith <davesmith.00000@gmail.com>
2020-11-18 09:28:43 +0000

    Fixing tests

--- moonshot/test/src/com/moonshot/model/ShipTests.scala
+++ moonshot/test/src/com/moonshot/model/ShipTests.scala
@@ -13,10 +13,10 @@ object ShipTests extends TestSuite {
       "Ship movement" - {
 
         "should move ship to the right after a full second" - {
-          val ship     = Ship(1, Vector2(10, 10), Vector2.zero, Vector2.zero)
+          val ship     = Ship(1, 1, Vector2(10, 10), Vector2.zero, Vector2.zero)
           val gameTime = GameTime(Seconds(1), Seconds(1), FPS.`60`)
 
-          val expected = Vector2(15, 10)
+          val expected = Vector2(160, 10)
 
           val actual = ship.moveRight().update(gameTime, Nil).coords
 
@@ -24,14 +24,13 @@ object ShipTests extends TestSuite {
         }
 
         "should move ship to the right after a single frame" - {
-          val ship     = Ship(1, Vector2(10, 10), Vector2.zero, Vector2.zero)
+          val ship     = Ship(1, 1, Vector2(10, 10), Vector2.zero, Vector2.zero)
           val gameTime = GameTime(Seconds(1), Seconds(0.01666666667), FPS.`60`)
-
-          val expected = Vector2(10.08333333333, 10)
 
           val actual = ship.moveRight().update(gameTime, Nil).coords
 
-          actual ==> expected
+          actual.x > ship.coords.x ==> true
+          (actual.x > 11.0 && actual.x < 12.0) ==> true
         }
 
       }
